0879345fffb763703746e7d97fcfce1407d9ebdd
Juan Gerardo <jmendez1025@cue.edu.co>
2024-07-07 22:22:01 -0500

    feat: error fetching implemented

---
+++ app/products/error.tsx
@@ -0,0 +1,10 @@
+"use client"
+function error() {
+  return (
+    <div className="bg-slate-950 min-h-screen flex justify-center items-center ">
+        Something went wrong.
+    </div>
+  )
+}
+
+export default error
--- app/products/loading.tsx
+++ app/products/loading.tsx
@@ -1,7 +1,7 @@
 
 export default function loading() {
   return (
-    <div className="bg-slate-950 min-h-screen">Loading products...</div>
+    <div className="bg-slate-950 min-h-screen flex justify-center items-center">Loading products...</div>
   )
 }
 
